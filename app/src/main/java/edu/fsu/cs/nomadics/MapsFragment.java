f8320b36446b58934d4ffc7258b1f04ebf427764
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2020-04-23 19:40:25 -0400

    Retrieving Current Location and Set Up Marker's
    -Can now retrieve the latlong of current location although I now realize how unnecessary it was
    -Is set up to recieve coordinates, place a marker on said coordinates, and zoom into that location.

--- app/src/main/java/edu/fsu/cs/nomadics/MapsFragment.java
+++ app/src/main/java/edu/fsu/cs/nomadics/MapsFragment.java
@@ -5,26 +5,35 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private OnMapsInteractionListener mListener;
     private GoogleMap map;
     private MapView mapView;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1346;
 
@@ -56,9 +65,30 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         MapsInitializer.initialize(getContext());
         map = googleMap;
 
+        // Checking to make sure location permissions have been granted and forcing user back to home page if they are not
         if (ActivityCompat.checkSelfPermission(getActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+            // getting the devices current location.
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            Task location = fusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()) {
+                        Location currentLocation = (Location) task.getResult();
+                    }
+                    else
+                        Toast.makeText(getActivity(), "Unable to find current location", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // will grab coordinates sent from other fragments and set marker here.
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(25.9117,-80.3185))
+                    .title("Name of Place"));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(25.9117,-80.3185), 10f));
+
             map.setMyLocationEnabled(true);
         } else {
             new AlertDialog.Builder(getActivity())
